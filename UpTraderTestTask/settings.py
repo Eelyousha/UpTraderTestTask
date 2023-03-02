d2e0197b84dc8bc0fe5dd3527ffcdcc81d279091
Ilyoushaa <krupeniv2521@gmail.com>
2023-03-02 17:52:10 +0300

    Fixed secret key

--- UpTraderTestTask/settings.py
+++ UpTraderTestTask/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hqm2so-cp*huu&w=)kjhn_igb)!k-@x2_1!$ky+g2-7fvgg8^h'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
